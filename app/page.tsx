1ccffea507472618ef9cb5b5ec09e0467aad7100
Jusce <jusce240@gmail.com>
2025-06-02 14:55:31 -0500

    add ctasection

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import { AboutScroll } from "@/components/AboutScroll"
 import { MissionPage } from '@/components/Mission'
 import { MissionPageScroll } from '@/components/MissionScrollHorizontal'
 import { Mission2 } from '@/components/Mission2'
-import CardScroll from '@/components/CardScrollTest'
+import Memberships from '@/components/Memberships'
+import { CTASection } from "@/components/CTASection"
 
 
 export default function Home() {
@@ -15,8 +16,8 @@ export default function Home() {
     <AboutScroll />
     <MissionPageScroll />
     <Mission2 />    
-    <CardScroll />
-
+    <Memberships />
+    <CTASection />
   </>
   );
 }
---
+++ components/CTASection.jsx
@@ -0,0 +1,11 @@
+import alexerPro from '@/lib/fonts';
+
+export const CTASection = () => {
+
+    return (
+        <div className="grid grid-cols-12 gap-[24px] px-[64px] h-[532px] bg-black">
+            <h2 className={`${alexerPro.className} text-8xl col-span-7 col-start-1 text-white mt-[76px]`}>UNETE A ERÖS CLUB</h2>
+        </div>
+    )
+
+}
